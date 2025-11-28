f3012b76a60ece5eee4e26ea0241f2b5581e3c99
Stan Matrosov <matrosovdream@gmail.com>
2025-11-28 19:46:26 +0700

    References class, initial version with routes

---
+++ references.php
@@ -0,0 +1,41 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class FrmHistoryReferences {
+
+    public function getApiRoutes(): array {
+        return [
+            'api_status' => [
+                'method' => 'GET',
+                'path'   => '/status',
+            ],
+            'api_account_status' => [
+                'method' => 'POST',
+                'path'   => '/account/status',
+            ],
+            'update_fields' => [
+                'method' => 'POST',
+                'path'   => '/entry-history',
+            ],
+            'update_history' => [
+                'method' => 'POST',
+                'path'   => '/entry-history',
+            ],
+            'get_history' => [
+                'method' => 'POST',
+                'path'   => '/entry-history',
+            ],
+        ];
+    }
+
+    public function getRoute( string $key ): ?array {
+
+        $routes = $this->getApiRoutes();
+        return isset( $routes[ $key ] ) ? $routes[ $key ] : null;
+
+    }
+    
+}
